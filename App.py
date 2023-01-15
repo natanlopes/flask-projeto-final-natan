c3b1f23251f70ae6a8006313990a22641dda815f
natanlopes <natan1918@gmail.com>
2023-01-15 10:20:47 -0300

    conncetion

--- App.py
+++ App.py
@@ -1,6 +1,29 @@
-from flask import Flask,render_template
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'Secret Key'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:''@localhost/crud'
+app.config['SQLALCHEMY TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# db.init_app(app)
+
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    tefone = db.Column(db.String(100))
+
+
+def __init__(self, nome, email, telfone):
+    self.nome = nome
+    self.email = email
+    self.tefone = telfone
 
 
 @app.route('/')
@@ -9,4 +32,5 @@ def Index():
 
 
 if __name__ == "__main__":
+
     app.run(debug=True)
