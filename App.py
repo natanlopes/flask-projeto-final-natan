516820db8fbd135a359f5e595529fc9664280c05
natanlopes <natan1918@gmail.com>
2023-01-15 22:36:22 -0300

    add crud

--- App.py
+++ App.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,20 +16,57 @@ class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
     email = db.Column(db.String(100))
-    tefone = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
 
 
-def __init__(self, nome, email, telfone):
-    self.nome = nome
-    self.email = email
-    self.tefone = telfone
+    def __init__(self, nome, email, phone):
+        self.nome = nome
+        self.email = email
+        self.phone = phone
 
 
 @app.route('/')
 def Index():
-    return render_template('index.html')
+    all_data = Data.query.all()
+    return render_template('index.html', empregar=all_data)
 
 
+@app.route('/insert', methods=["POST"])
+def insert():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        my_data = Data(nome, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+        flash('Funcionario adcionado com sucesso')
+        return redirect(url_for('Index'))
+
+
+@app.route('/update', methods=['GET','POST'])
+def update():
+    if request.method == 'POST':
+        my_data = Data.query.get(request.form.get('id'))
+        my_data.nome = request.form['nome']
+        my_data.email = request.form['nome']
+        my_data.phone = request.form['nome']
+
+        db.session.commit()
+        flash('Funcionarios atualizado com sucesso')
+        return redirect(url_for('Index'))
+
+
+@app.route('/delete/<id>/', methods=['GET', 'POST'])
+def delete(id):
+    my_data = Data.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash('Empregado excluido com sucesso')
+
+    return redirect(url_for('Index'))
 if __name__ == "__main__":
-
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
